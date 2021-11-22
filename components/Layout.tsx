aebe005b9383d9f12aecab176d48c9bc07664f32
gagan <gagandaroach@gmail.com>
2021-11-21 18:41:06 -0600

    use layout, add about page, nav

---
+++ components/Layout.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import styles from '../styles/Layout.module.css'
+
+
+type Props = {
+  children?: ReactNode
+  title?: string
+}
+
+const Layout = ({ children, title = 'www.daroach.net' }: Props) => (
+  <div className={styles.main}>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content="my contribution to the virtual world" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet="utf-8" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        {' | '}
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
+    </header>
+    {children}
+    <footer className={styles.footer}>
+      {/* <span>www.daroach.net - running on a high performance webserver in my apartment</span> */}
+      <span>Gagan Daroach &copy; 2019-2021</span>
+      <span><a href="https://github.com/gagandaroach/daroachnet" rel="noreferrer" target="_blank">Source Code</a></span>
+      <span><a href="mailto:gagandaroach@gmail.com?Subject=www.daroach.net contact me&Body=Hello Gagan, I like daroach.net!" rel="noreferrer" target="_blank">Contact Me</a></span>
+    </footer>
+  </div>
+)
+
+export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,64 +2,48 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>www.daroach.net - Home</title>
-        <meta name="description" content="my contribution to the virtual world" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <div className={styles.banner}>
-          <h1 className={styles.title}>Welcome to <a href="">Daroach.net!</a></h1>
-          <h2>You have used the power of the internet to connect to a <a
-            href="https://www.instagram.com/p/BtSgNa8ni0i/?utm_source=ig_web_options_share_sheet"
-            target="_blank" rel="noopener noreferrer">webserver</a> running in my apartment.
-          </h2>
-        </div>
-
-        <div className={styles.content}>
-          <div className={styles.sec}>
-            <h1 className={styles.secHeader}>Web traffic</h1>
-          </div>
-
-          {/* <div className={styles.sec}>
-            <h1 className={styles.secHeader}>My blog</h1>
-          </div> */}
-
-          <div className={styles.sec}>
-            <h1 className={styles.secHeader}>About me</h1>
-          </div>
-
-          <div className={styles.sec}>
-            <h1 className={styles.secHeader}>Web services</h1>
-          </div>
-
-          <div className={styles.sec}>
-            <h1 className={styles.secHeader}>Debug</h1>
-            <div className="flex text-center space-x-2">
-              <div className="bg-gray-200 p-5 hover:bg-green-500">
-                <Link href="/api" >Link to api</Link>
-              </div>
-              <div className="bg-gray-200 p-5 hover:bg-green-500">
-                <Link href="/api/hits" >Link to api/hits</Link>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-        <footer className={styles.footer}>
-          {/* <span>www.daroach.net - running on a high performance webserver in my apartment</span> */}
-          <span>Gagan Daroach &copy; 2019-2021</span>
-          <span><a href="https://github.com/gagandaroach/daroachnet" rel="noreferrer" target="_blank">Source Code</a></span>
-          <span><a href="mailto:gagandaroach@gmail.com?Subject=www.daroach.net contact me&Body=Hello Gagan, I like daroach.net!" rel="noreferrer" target="_blank">Contact Me</a></span>
-        </footer>
-      </main>
-
+const HomePage = () => (
+  <Layout title="Home | www.daroach.net">
+    <div className={styles.banner}>
+      <h1 className={styles.title}>Welcome to <a href="">Daroach.net!</a></h1>
+      <h2>You have used the power of the internet to connect to a <a
+        href="https://www.instagram.com/p/BtSgNa8ni0i/?utm_source=ig_web_options_share_sheet"
+        target="_blank" rel="noopener noreferrer">webserver</a> running in my apartment.
+      </h2>
     </div>
-  )
-}
+
+    <div className={styles.content}>
+      <div className={styles.sec}>
+        <h1 className={styles.secHeader}>Web traffic</h1>
+      </div>
+
+      {/* <div className={styles.sec}>
+      <h1 className={styles.secHeader}>My blog</h1>
+    </div> */}
+
+      <div className={styles.sec}>
+        <h1 className={styles.secHeader}>About me</h1>
+      </div>
+
+      <div className={styles.sec}>
+        <h1 className={styles.secHeader}>Web services</h1>
+      </div>
+
+      <div className={styles.sec}>
+        <h1 className={styles.secHeader}>Debug</h1>
+        <div className="flex text-center space-x-2">
+          <div className="bg-gray-200 p-5 hover:bg-green-500">
+            <Link href="/api" >Link to api</Link>
+          </div>
+          <div className="bg-gray-200 p-5 hover:bg-green-500">
+            <Link href="/api/hits" >Link to api/hits</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+)
+export default HomePage
