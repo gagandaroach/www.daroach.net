1a9b4db9053e2108a99e55bd074522dfeab3a264
gagany <gagandaroach@gmail.com>
2023-08-13 11:21:38 +0000

    remove extra stuff from default.vue

--- www/layouts/default.vue
+++ www/layouts/default.vue
@@ -1,37 +1,7 @@
 <!-- https://nuxt.com/docs/getting-started/views#layouts -->
 <template>
 <div class="">
-  <div class="flex flex-col bg-black min-h-screen min-w-screen">
-    <div :class="headerClass">
-      <div>
-
-        <div class="flex flex-col">
-          <div class="flex flex-row space-x-1">
-            <NuxtLink to="/" :class="headerTitleClass" :active-class="headerTitleActiveClass">daroach.net</NuxtLink>
-          </div>
-
-          <div class="flex flex-col space-y-1">
-            <p :class="headerPlugClass"></p>
-          </div>
-
-        </div>
-
-      </div>
-  
-      <div class="flex space-x-2">
-        <!-- <div>
-          <NuxtLink to="/teams" :class="buttonClass" :active-class="buttonActiveClass">Teams</NuxtLink>
-        </div>
-        <div>
-          <NuxtLink to="/games" :class="buttonClass" :active-class="buttonActiveClass">Games</NuxtLink>
-        </div>
-        <div>
-          <NuxtLink to="/scoreboard" :class="buttonClass" :active-class="buttonActiveClass">Scoreboard</NuxtLink>
-        </div> -->
-      </div>
-    </div>
-
-    
+  <div class="flex flex-col bg-stone-500 min-h-screen min-w-screen">
     <slot />
   </div>
 </div>
@@ -51,15 +21,6 @@ useHead({
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
  ]
 });
-
-const headerClass = "flex justify-between text-white md:p-3 p-1";
-const headerTitleClass = "flex md:text-4xl text-base";
-const headerPlugClass = "md:text-base text-xs tracking-widest";
-const headerTitleActiveClass = ""
-const buttonCommonClass = "flex text-center md:text-lg text-xs md:p-3 p-1";
-const buttonClass = `${buttonCommonClass} my-2 self-center`;
-const buttonActiveClass = `${buttonCommonClass} text-hsu-red`;
-
 </script>
 
 <style>
