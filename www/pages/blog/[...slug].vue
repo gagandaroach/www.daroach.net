27477f899b5b43917974317446c2949688db7cf2
gagany <gagandaroach@gmail.com>
2023-08-15 09:51:54 +0000

    elementary blog styling

--- www/pages/blog/[...slug].vue
+++ www/pages/blog/[...slug].vue
@@ -1,5 +1,28 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc class="dnet-blog-content"/>
   </main>
 </template>
+
+<style>
+.dnet-blog-content {
+  @apply p-4 space-y-2;
+}
+
+h1 {
+  font-family: 'Ubuntu Mono';
+  @apply text-center;
+  @apply text-stone-50 text-xl;
+}
+
+p {
+  font-family: 'Ubuntu';
+  @apply text-stone-100;
+}
+
+a {
+  font-family: 'Ubuntu Condensed';
+  @apply text-red-300;
+}
+
+</style>
