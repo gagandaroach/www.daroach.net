8af6b9399f0f273b56fcbd1c67e77915e87dac5e
gagan <gagandaroach@gmail.com>
2024-02-19 15:58:52 -0800

    center navbar

--- www/components/NavBar.vue
+++ www/components/NavBar.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="dnet-navbar flex flex-row justify-between">
-        <div class="flex flex-col">
-            <NuxtLink to="/" class="dnet-navbar-text-main">daroach.net</NuxtLink>
-            <Breadcrumbs />
-        </div>
-        <div class="flex space-x-1">
-            <NuxtLink v-for="(b, index) in navButtons" :to="b.url" :key="index" class="dnet-button-nav"> {{ b.label }}
-            </NuxtLink>
+    <div class="dnet-navbar">
+        <div class="flex flex-row justify-between max-w-screen-xl m-auto">
+            <div class="flex flex-col">
+                <NuxtLink to="/" class="dnet-navbar-text-main">daroach.net</NuxtLink>
+                <Breadcrumbs />
+            </div>
+            <div class="flex space-x-1">
+                <NuxtLink v-for="(b, index) in navButtons" :to="b.url" :key="index" class="dnet-button-nav"> {{ b.label }}
+                </NuxtLink>
+            </div>
         </div>
     </div>
 </template>
