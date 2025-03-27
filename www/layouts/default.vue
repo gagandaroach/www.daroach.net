e85550451909016c239c8c6ba7e4ffb5583618b3
gagany <gagandaroach@gmail.com>
2025-03-27 15:39:16 -0700

    code refactor; add footer

--- www/layouts/default.vue
+++ www/layouts/default.vue
@@ -1,26 +1,51 @@
-<!-- https://nuxt.com/docs/getting-started/views#layouts -->
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useSettingsStore } from '~/stores/settings' // Adjust path if needed
 
+const settingsStore = useSettingsStore()
+const { _bDebugButtons } = storeToRefs(settingsStore)
 
-<script setup>
-const settingsStore = useSettingsStore();
-const { _bDebugButtons, bRedirectToWelcome, bCookieConsent } = storeToRefs(settingsStore);
+// --- Title Management ---
+const webpageMainTitle = 'daroach.net'
+const localTitleChunk = useState<string>('title', () => webpageMainTitle)
 
-// Set up the page title information
-const webpageMainTitle = "daroach.net"
-const localTitleChunk = useState('title', () => webpageMainTitle)
 useHead({
-  titleTemplate: (titleChunk) => {
+  titleTemplate: (titleChunk?: string) => {
     const newTitle = titleChunk ? `${titleChunk} - ${webpageMainTitle}` : webpageMainTitle
-    localTitleChunk.value = titleChunk ? `${webpageMainTitle} - ${titleChunk}` : webpageMainTitle;
-    return newTitle;
+    localTitleChunk.value = newTitle // Update state for potential other uses
+
+    return newTitle
   },
-  link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
-  ]
-});
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }]
+})
 </script>
 
-<style>
+<template>
+  <div class="dnet-layout h-full min-h-screen dnet-bg m-auto">
+    <div class="dnet-layout-content flex flex-col">
+      <TheNavBar />
+      <CookieConsent />
+
+      <main class="dnet-layout-main">
+        <slot />
+      </main>
+
+      <DevOnly>
+        <div v-if="_bDebugButtons" class="dnet-debug-buttons bg-stone-800 border-4 border-green-300 m-auto">
+          <DebugButtons />
+        </div>
+      </DevOnly>
+    </div>
+    <TheFooter />
+  </div>
+</template>
+
+<style scoped>
+.dnet-layout-main {
+  @apply transition-opacity duration-200; /* Apply transition to main content */
+}
+
+/* Nuxt Page Transitions */
 .page-enter-active,
 .page-leave-active {
   transition: all 0.20s;
@@ -32,19 +57,3 @@ useHead({
   filter: blur(1rem);
 }
 </style>
-
-
-<template>
-  <div class="h-full min-h-screen dnet-bg m-auto">
-    <div class="flex flex-col">
-      <NavBar />
-      <CookieConsent />
-      <slot />
-      <DevOnly>
-        <div v-if="_bDebugButtons" class="bg-stone-800 border-4 border-green-300 m-auto">
-          <DebugButtons />
-        </div>
-      </DevOnly>
-    </div>
-  </div>
-</template>
--- www/nuxt.config.ts
+++ www/nuxt.config.ts
@@ -1,58 +1,58 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
+  postcss: {
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+      },
+  },
 
-    css: [
-        '~/assets/css/main.css',
-        '~/assets/css/dnet.css',
-        '~/assets/css/dnet-bg.css',
-        '~/assets/css/dnet-blog.css',
-        '~/assets/css/dnet-navbar.css',
-        '~/assets/css/dnet-text.css'
-    ],
+  css: [
+      '~/assets/css/main.css',
+      '~/assets/css/dnet.css',
+      '~/assets/css/dnet-bg.css',
+      '~/assets/css/dnet-blog.css',
+      '~/assets/css/dnet-text.css'
+  ],
 
-    components: [
-        { path: '~/components/Card', prefix: '' },
-        { path: '~/components' }
-    ],
+  components: [
+      { path: '~/components/Card', prefix: '' },
+      { path: '~/components/global', prefix: '' },
+      { path: '~/components' }
+  ],
 
-    // https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
-    runtimeConfig: {
-        public: { apiBase: '/api' }
-    },
+  // https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
+  runtimeConfig: {
+      public: { apiBase: '/api' }
+  },
 
+  app: {
+      pageTransition:
+      {
+          name: 'page', mode: 'out-in'
+      }
+  },
 
-    app: {
-        pageTransition:
-        {
-            name: 'page', mode: 'out-in'
-        }
-    },
+  modules: [
+      '@nuxt/image',
+      '@nuxt/content',
+      '@pinia/nuxt',
+      ['@nuxtjs/google-fonts', {
+          families: {
+            Ubuntu: true,
+            'Ubuntu+Condensed': true,
+            'Ubuntu+Mono': true,
+          }
+      }]
+  ],
 
-    modules: [
-        '@nuxt/image',
-        '@nuxt/content',
-        '@pinia/nuxt',
-        ['@nuxtjs/google-fonts', {
-            families: {
-              Ubuntu: true,
-              'Ubuntu+Condensed': true,
-              'Ubuntu+Mono': true,
-            }
-        }]
-    ],
-    
-    // https://content.nuxtjs.org/
-    content: {
-        // documentDriven: true
-        // experimental: {
-        //     search: true
-        // }
-    }
+  // https://content.nuxtjs.org/
+  content: {
+      // documentDriven: true
+      // experimental: {
+      //     search: true
+      // }
+  },
 
+  compatibilityDate: '2025-03-27'
 })
